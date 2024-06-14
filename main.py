28b0b74208c728ae9c80135a93a537e5393d32e2
Vodovva <valerievodovnik@gmail.com>
2024-06-14 12:45:43 -0500

    lesson 4 completed

--- main.py
+++ main.py
@@ -179,32 +179,77 @@
 
 
 
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
+# bill = 0
+
+# if height >= 120:
+#     print('You can ride the rollercoaster!')
+#     age = int(input('What is your age?'))
+#     if age < 12:
+#         bill = 5
+#         print('Child tickets are $5.')
+#     elif age <= 18:
+#         bill = 7
+#         print('Youth tickets are $7')
+#     else:
+#         bill = 12
+#         print('Adult tickets are $12.')
+        
+#     wants_photo = input('Do upi want a photo taken? Y or N.')
+#     if wants_photo == 'Y':
+#         #Add $3 to their bill
+#         bill += 3
+
+#     print(f'Your final bill is {bill}')
+
+# else:
+#     print('Sorry you have to grow taller before you can ride.')
+
+
+# Lesson 4
+# Congratulations, you've got a job at Python Pizza! Your first job is to build an automatic pizza order program.
+# Based on a user's order, work out their final bill.
+# Small pizza (S): $15
+# Medium pizza (M): $20
+# Large pizza (L): $25
+# Add pepperoni for small pizza (Y or N): +$2
+# Add pepperoni for medium or large pizza (Y or N): +$3
+# Add extra cheese for any size pizza (Y or N): +$1
+
+
+print("Thank you for choosing Python Pizza Deliveries!")
+size = input() # What size pizza do you want? S, M, or L
+add_pepperoni = input() # Do you want pepperoni? Y or N
+extra_cheese = input() # Do you want extra cheese? Y or N
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
 bill = 0
 
-if height >= 120:
-    print('You can ride the rollercoaster!')
-    age = int(input('What is your age?'))
-    if age < 12:
-        bill = 5
-        print('Child tickets are $5.')
-    elif age <= 18:
-        bill = 7
-        print('Youth tickets are $7')
-    else:
-        bill = 12
-        print('Adult tickets are $12.')
-        
-    wants_photo = input('Do upi want a photo taken? Y or N.')
-    if wants_photo == 'Y':
-        #Add $3 to their bill
-        bill += 3
-
-    print(f'Your final bill is {bill}')
-    
+if size == 'S':
+    bill = 15
+elif size == 'M':
+    bill = 20
 else:
-    print('Sorry you have to grow taller before you can ride.')
+    bill += 25
+
+
+if add_pepperoni == 'Y':
+    if size == 'S':
+        bill += 2
+    else:
+        bill += 3
+if extra_cheese == 'Y':
+    bill += 1
+
+print(f'Your bill is ${bill}')
+
+
+
+
+
+
+
 
 
 
